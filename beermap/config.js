8c913ca3d53722499413ac9cd6a0d2cedcc0548d
cmboone2005 <56392920+cmboone2005@users.noreply.github.com>
2020-03-26 17:10:52 -0400

    1st push

---
+++ beermap/config.js
@@ -0,0 +1,3 @@
+// API key
+const API_KEY = "pk.eyJ1IjoiY21ib29uZTIwMDUiLCJhIjoiY2s3d2hqNTJwMDIybDNqbG5vaHhpbTV6biJ9.CXopDAMC8clWafhdOuxlPw";
+
---
+++ beermap/map.js
@@ -0,0 +1,65 @@
+// Creating map object
+var myMap = L.map("map", {
+    center: [35.78, -78.64],
+    zoom: 11
+    layers: [lightmap, breweryLocation]
+  });
+
+ // Adding tile layer to the map
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+    maxZoom: 18,
+    id: "mapbox.streets",
+    accessToken: API_KEY
+  }).addTo(myMap);
+
+var baseMaps = {
+  "Light Map" : lightmap
+};
+
+// var overlayMaps = {
+//   "Breweries" : breweryLocation
+// };
+
+var dataFile ="fill in data file"
+
+function csvJSON(geoData){
+
+    var lines=dataFile.split("\n");
+  
+    var geoData = [];
+  
+    var headers=lines[0].split(",");
+  
+    for(var i=1;i<lines.length;i++){
+  
+        var obj = {};
+        var currentline=lines[i].split(",");
+  
+        for(var j=0;j<headers.length;j++){
+            obj[headers[j]] = currentline[j];
+        }
+  
+        geoData.push(obj);
+  
+    }
+  
+    //return result; //JavaScript object
+    return JSON.stringify(geoData); //JSON
+  }
+
+function createMarkers(response) {
+  var breweries = response.geoData.breweries;
+  var breweryLoc = [];
+  for (var index = 0; index < breweries.length; index ++) {
+    var breweryLoc = L.marker([breweries.lat, breweries.lon])
+    breweryLoc.push(breweryLoc);
+  }
+  createMap(L.layerGroup(breweryLoc));
+}
+
+d3.json(geoData, createMarkers);
+
+  
+
+
