598cc751bbecbbe60e84aab28759438b761a7738
Brendan Dowd <54948473+bdowd91@users.noreply.github.com>
2020-04-02 18:42:07 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import csv
+import json
+import numpy as np
+import sqlite3
+from sqlalchemy import create_engine
+import pandas as pd 
+
+app = Flask(__name__)
+
+file = '..\north_carolina_brweries\assets\data\nc_breweries_df.csv'
+
+breweries_db = create_engine('sqlite:///breweries_db.db')
+
+for df in pd.read_csv(file)
+
+@app.route('/', methods=["GET", "POST "])
+def homepage():
+      data.brewery = request.form.get('breweries', '')
+      data.city = request.form.get('city', '')
+
+      df = pd.read_csv('..\north_carolina_brweries\assets\data\nc_breweries_df.csv')
+      brewery = data.brewery
+      city = data.city
+
+      df = df[df.brewery == brewery]
+      df = df[df.city == city]
+
+      df = df[["breweries", "city"]]
+
+if __name__ == "__main__":
+    app.run(debug=True)
