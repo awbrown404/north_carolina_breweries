bbf0f007f24f0f2fdc72a3a3819e1612ee5247f9
speculadora <spencerharrison0@gmail.com>
2020-04-03 18:25:23 -0400

    File reorganization, changes to beerList files

--- application.py
+++ application.py
@@ -1,42 +1,48 @@
-# import Flask
-from flask import Flask, render_template, request, redirect, url_for
-from pymongo import MongoClient
+# import dependencies
+from flask import Flask, render_template
+import pymongo
+import pandas as pd
 
-# create a Flask app
-app = Flask(__name__)
-conn = MongoClient('mongodb://localhost:27017')
+# read in data
+master = pd.read_csv("data/master_beer_df.csv")
+master_condensed = pd.read_csv("data/master_beer_condensed.csv")
+breweries = pd.read_csv("data/nc_breweries_df.csv")
+breweries_condensed = pd.read_csv("data/satallite_breweries_removed.csv")
+
+# establish mongo db connection
+conn = 'mongodb://localhost:27017'
 client = pymongo.MongoClient(conn)
-
-# Define the database in Mongo
-db = client.beers_project
+db = client.nc_breweries_db
 
 # drop existing collection to prevent duplicates
 db.beer_master.drop()
+db.master_condensed.drop()
 db.breweries.drop()
+db.breweries_condensed.drop()
 
-# create a collection and insert data
+# creates a collection and inserts data
 db.beer_master.insert_many(master.to_dict('records'))
+db.master_condensed.insert_many(master_condensed.to_dict('records'))
 db.breweries.insert_many(breweries.to_dict('records'))
+db.breweries_condensed.insert_many(breweries_condensed.to_dict('records'))
 
-# route to "landing" page
+app = Flask(__name__)
+
 @app.route('/')
-def index():
-        return render_template('index.html')
+def main():
+    return render_template("index.html")
 
-#route to beer list page
 @app.route('/beerList')
 def beerList():
-    master =list(db.beer_master.find())
-    breweries = list(db.breweries.find()
+    master = list(db.master_condensed.find({}, {'_id': False}))
+    breweries = list(db.breweries_condensed.find({}, {'_id': False}))
     return render_template("beerList.html", master=master, breweries=breweries)
 
-#route to beer map page
 @app.route('/beerMap')
 def beerMap():
-    master = list(db.beer_master.find())
-    breweries = list(db.breweries.find())
+    master = list(db.beer_master.find({}, {'_id': False}))
+    breweries = list(db.breweries.find({}, {'_id': False}))
     return render_template("beerMap.html", master=master, breweries=breweries)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
