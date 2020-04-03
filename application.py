c358ec9e25e259c48de1473cfe98130e389010dc
Brendan Dowd <54948473+bdowd91@users.noreply.github.com>
2020-04-03 15:37:54 -0400

    Update application.py

--- application.py
+++ application.py
@@ -4,15 +4,19 @@ from pymongo import MongoClient
 
 # create a Flask app
 app = Flask(__name__)
-client = MongoClient('mongodb://localhost:27017')
+conn = MongoClient('mongodb://localhost:27017')
+client = pymongo.MongoClient(conn)
 
 # Define the database in Mongo
 db = client.beers_project
 
-# Collections
-breweries = db.breweries
-beer_list = db.beers
-master_beer = db.master_beer
+# drop existing collection to prevent duplicates
+db.beer_master.drop()
+db.breweries.drop()
+
+# create a collection and insert data
+db.beer_master.insert_many(master.to_dict('records'))
+db.breweries.insert_many(breweries.to_dict('records'))
 
 # route to "landing" page
 @app.route('/')
@@ -22,12 +26,16 @@ def index():
 #route to beer list page
 @app.route('/beerList')
 def beerList():
-    return render_template('beerList.html')
+    master =list(db.beer_master.find())
+    breweries = list(db.breweries.find()
+    return render_template("beerList.html", master=master, breweries=breweries)
 
 #route to beer map page
 @app.route('/beerMap')
 def beerMap():
-    return render_template('beerMap.html')
+    master = list(db.beer_master.find())
+    breweries = list(db.breweries.find())
+    return render_template("beerMap.html", master=master, breweries=breweries)
 
 
 if __name__ == '__main__':
